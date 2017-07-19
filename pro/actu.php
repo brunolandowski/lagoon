2215f2d7b98c41b3c3c5eae73bf75f64ec4523a5
Bruno Landowski <brunolandowski@Brunos-MacBook-Pro.local>
2017-07-20 00:25:56 +0200

    actu

---
+++ pro/actu.php
@@ -0,0 +1,85 @@
+<?php 
+
+$pageCSS = 'actu.css';
+$className = 'actu';
+
+include "header.php";
+?>
+    <div class="title">
+        <h1>ACTUALITÉS</h1>
+    </div> 
+
+<div class="bloclist">
+    <div class="content">
+        
+        <div class="filters">
+            <div class="select">
+                <select name="" id="">
+                    <option selected="selected">CATÉGORIES</option>
+                </select>
+            </div>
+        </div>
+
+        <article>
+            <div class="left">
+                <h3>ACTUALITÉ PRO NUMÉRO 1</h3>
+                <span class="date">LE 12 MARS 2017 À 16h30</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim ex ut erat convallis venenatis. Mauris convallis urna vulputate, rutrum mi vel, placerat ligula. Quisque congue turpis sed velit molestie vehicula.</p>
+            </div>
+            <div class="right">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <button>EN SAVOIR PLUS</button>   
+            </div>
+        </article>
+
+          <article>
+            <div class="left">
+                <h3>ACTUALITÉ PRO NUMÉRO 1</h3>
+                <span class="date">LE 12 MARS 2017 À 16h30</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim ex ut erat convallis venenatis. Mauris convallis urna vulputate, rutrum mi vel, placerat ligula. Quisque congue turpis sed velit molestie vehicula.</p>
+            </div>
+            <div class="right">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <button>EN SAVOIR PLUS</button>   
+            </div>
+        </article>
+
+          <article>
+            <div class="left">
+                <h3>ACTUALITÉ PRO NUMÉRO 1</h3>
+                <span class="date">LE 12 MARS 2017 À 16h30</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim ex ut erat convallis venenatis. Mauris convallis urna vulputate, rutrum mi vel, placerat ligula. Quisque congue turpis sed velit molestie vehicula.</p>
+            </div>
+            <div class="right">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <button>EN SAVOIR PLUS</button>   
+            </div>
+        </article>
+    
+    </div>
+</div>
+
+<ul class="pagination">
+    <li class="page current">1</li>
+    <li class="page"><a href="#">2</a></li>
+    <li class="page"><a href="#">3</a></li>
+    <li class="page elipsis">...</li>
+    <li class="page"><a href="#">6</a></li>
+    <li class="page"><a href="#">7</a></li>
+    <li class="page"><a href="#">Suivants</a></li>
+</ul>
+<?php 
+include "footer.php";
+?>
