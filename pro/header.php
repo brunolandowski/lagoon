4ac3efe015b966ae5ac1e17ee57bfb9322c8dde0
Bruno Landowski <brunolandowski@Brunos-MacBook-Pro.local>
2017-07-22 15:03:50 +0200

    header

--- pro/header.php
+++ pro/header.php
@@ -4,7 +4,13 @@
         <header>
             <div class="topBar">
                 <a href="" class="logo">PRO</a>
-                <a href="index.php" class="logout">déconnexion</a>
+                <span class="right-nav">
+                    <a href="index.php"><div class="alert"><span>5</span></div></a>
+                    <a href="index.php"><div class="message"><span>12</span></div></a>
+                    <a href="index.php"><div class="user"></div></a>
+                </span>
+                
+
             </div>
             <ul class="nav">
                 <li class='active'><a href="home.php">ACCUEIL</a></li>
@@ -12,5 +18,15 @@
                 <li><a href="#">ÉVÉNEMENTS</a></li>
                 <li><a href="#">SUPPORTS MARKETING</a></li>
                 <li><a href="#">SERVICES CLIENT</a></li>
+            </ul>
+            <ul class="langs">
+                <li class="lang en active"><div class="actived">en</div></li>
+                <li class="lang fr rollable"><a href="#">fr</a></li>
+                <li class="lang de rollable"><a href="#">de</a></li>
+                <li class="lang es rollable"><a href="#">es</a></li>
+                <li class="lang it rollable"><a href="#">it</a></li>
+                <li class="lang pt rollable"><a href="#">pt</a></li>
+                <li class="lang ru rollable"><a href="#">ru</a></li>
+                <li class="lang cn rollable"><a href="#">cn</a></li>
             </ul>
         </header>
